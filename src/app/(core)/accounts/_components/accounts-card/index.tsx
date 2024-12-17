a9f84889bd93032877c43a336aa8e60c166f8902
lucas.dalan <lucasdalan09@gmail.com>
2024-12-17 16:12:09 -0300

    feat(accounts-card): added

--- src/app/(core)/accounts/_components/accounts-card/index.tsx
+++ src/app/(core)/accounts/_components/accounts-card/index.tsx
@@ -1,5 +1,28 @@
+import { Button } from "@/components/button";
+import { Icon } from "@/components/icon";
+import { PencilLine } from "lucide-react";
+
 export const AccountsCard = ({
   account,
 }: {
   account: { id: number; name: string; balance: string; type: string };
-}) => <div className="rounded-sm border w-[350px] h-[200px]">alo</div>;
+}) => {
+  const { id, balance, name, type } = account;
+
+  return (
+    <div className="rounded-sm border w-[350px] h-[180px] relative flex items-end justify-between p-4">
+      <Button
+        className="absolute top-4 right-4"
+        size={"icon"}
+        variant={"secondary"}
+      >
+        <Icon src={PencilLine} />
+      </Button>
+      <div>
+        <h1 className="font-semibold">{name}</h1>
+        <p className="font-light">{type}</p>
+      </div>
+      {balance}
+    </div>
+  );
+};
