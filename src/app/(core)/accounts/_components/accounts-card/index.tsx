5814d245a110ef7255ebc8a919df1c8a10034d95
lucas.dalan <lucasdalan09@gmail.com>
2024-12-17 15:35:58 -0300

    account-card

---
+++ src/app/(core)/accounts/_components/accounts-card/index.tsx
@@ -0,0 +1,5 @@
+export const AccountsCard = ({
+  account,
+}: {
+  account: { id: number; name: string; balance: string; type: string };
+}) => <div className="rounded-sm border w-[350px] h-[200px]">alo</div>;
--- src/app/(core)/accounts/page.tsx
+++ src/app/(core)/accounts/page.tsx
@@ -1,5 +1,19 @@
-import { AccountsDataTable } from "./_components/accounts-data-table";
+import { accountService } from "@/services/account";
+import { AccountsCard } from "./_components/accounts-card";
 
-export default function Page() {
-  return <AccountsDataTable />;
+export default async function Page() {
+  const accounts = await accountService.getTableData();
+
+  const content = accounts.map((value) => (
+    <AccountsCard
+      account={value}
+      key={value.id}
+    />
+  ));
+
+  return (
+    <div className="h-full">
+      <div className="grid grid-cols-4 gap-y-8">{content}</div>
+    </div>
+  );
 }
