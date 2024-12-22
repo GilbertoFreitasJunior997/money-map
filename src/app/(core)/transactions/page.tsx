4743a4e1c04297e89d37dcbca2a45357c2808008
lucas.dalan <lucasdalan09@gmail.com>
2024-12-22 01:30:48 -0300

    feat(date-range-input): added date range input

--- src/app/(core)/transactions/page.tsx
+++ src/app/(core)/transactions/page.tsx
@@ -6,11 +6,13 @@ import { SummaryCardList } from "./_components/summary-card-list";
 import { TransactionList } from "./_components/transaction-list";
 import { TransactionListLoading } from "./_components/transaction-list-loading";
 import { getTransactionListData } from "./actions";
+import { DateRangeInput } from "@/components/date-range-input";
+import { DateRange } from "react-day-picker";
 
 export default function Page() {
-  const [period, _setPeriod] = useState({
-    start: new Date(new Date().setMonth(new Date().getMonth() - 1)),
-    end: new Date(),
+  const [period, _setPeriod] = useState<DateRange>({
+    from: new Date(new Date().setMonth(new Date().getMonth() - 1)),
+    to: new Date(),
   });
 
   const {
@@ -18,12 +20,17 @@ export default function Page() {
     isLoading,
     isFetching,
   } = useActionQuery({
-    action: () => getTransactionListData(period.start, period.end),
-    queryKey: ["transactions", period.start, period.end],
+    action: () => getTransactionListData(period.from, period.to),
+    queryKey: ["transactions", period.from, period.to],
   });
 
   return (
     <div className="space-y-6 py-2 px-1 sm:px-6 md:px-24">
+      <DateRangeInput
+        name=""
+        value={period}
+        onChange={_setPeriod}
+      />
       <SummaryCardList
         transactions={transactions}
         isLoading={isLoading}
--- src/components/date-input/types.ts
+++ src/components/date-input/types.ts
@@ -1,10 +1,12 @@
 import { FieldValues } from "react-hook-form";
 import { InputDefaultProps } from "../form/types";
 
-export type DateInputProps<TForm extends FieldValues> =
-  InputDefaultProps<TForm> & {
-    label?: string;
-    value?: Date;
-  };
+export type DateInputProps<TForm extends FieldValues> = OmitMerge<
+  InputHTMLAttributes<HTMLInputElement>,
+  InputDefaultProps<TForm>
+> & {
+  label?: string;
+  value?: Date;
+};
 
 export type DateInputRef = HTMLButtonElement;
